0458853030b15fcf7a11364b82e0f541b2a53d4c
Pupperoni <ryanchester98@yahoo.com>
2018-04-21 11:17:57 +0800

    Added constant obj

--- src/App.js
+++ src/App.js
@@ -2,7 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const ObjectTest = {
+  1: {
+      id : 1,
+      name:'ABC'
+  },
+  3: {
+      id: 3,
+      name:'DEF'
+  }
+}
+
+
 class App extends Component {
+  _renderObject(){
+		return Object.entries(ObjectTest).map(([key, value], i) => {
+			return (
+				<div key={key}>
+					id is: {value.id} ;
+					name is: {value.name}
+				</div>
+			)
+		})
+  }
+  
   render() {
     return (
       <div className="App">
@@ -13,9 +36,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <p>This is an Object</p>
+          {this._renderObject()}
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
